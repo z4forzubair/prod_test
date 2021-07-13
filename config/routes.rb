ef014657cc1c6424f78060b6810fe56a98cf4a71
M Zubair <zubairpunjab786@gmail.com>
2021-07-13 20:57:58 +0500

    removed carts controller n view

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  root to: 'carts#index'
-  get 'cart', to: 'carts#index'
+  root to: 'shops#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
