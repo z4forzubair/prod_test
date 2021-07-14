417827270a76fd653725874d3c12a1e8f3d9a4ff
M Zubair <zubairpunjab786@gmail.com>
2021-07-15 02:37:56 +0500

    edit shop and categories

--- app/services/shop_category.rb
+++ app/services/shop_category.rb
@@ -4,12 +4,15 @@ class ShopCategory
   def initialize(shop = nil, services = nil); end
 
   def save_categories(shop, services)
-    debugger
     flag = true
     services.drop(1).each do |service|
       category = Category.new(service: service, shop_id: shop.id)
       flag = false unless category.save
     end
     flag
+  end
+
+  def delete_old_categories(shop)
+    shop.categories.map(&:destroy)
   end
 end
