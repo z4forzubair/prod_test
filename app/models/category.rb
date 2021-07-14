10aeb98645f74273d7ce476eb2d5c459080f869b
M Zubair <zubairpunjab786@gmail.com>
2021-07-14 17:37:45 +0500

    added validations for shop and category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   belongs_to :shop
+
+  validates :category, presence: true
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,6 @@
 class Shop < ApplicationRecord
   belongs_to :user, optional: true
   has_many :categories, dependent: :destroy
+
+  validates :name, :description, presence: true
 end
